1e213c33721cb5ecf4ddc52744254476637ed8d5
Grant James <grant.james@cardinalhealth.com>
2016-07-15 14:34:59 -0400

    wip

--- servo_controller.js
+++ servo_controller.js
@@ -5,6 +5,23 @@ module.exports = function(gpioServer, pin){
     S.gpioServer = gpioServer;
     S.pin = pin;
 
+    S.setAngleRestrictions = function(min, max) {
+        min = Math.abs(min) || 0;
+        max = Math.abs(max) || 0;
+        min = min*1;
+        max = max*1;
+        min = min>179 ? 0 : min;
+        max = max>180 ? 180 : max;
+        max = max==min ? min+1 : max;
+        if(max < min) {
+            var m = min;
+            min = max;
+            max = m;
+        }
+        S.angler.restrictions.angle.min = min;
+        S.angler.restrictions.angle.max = max;
+    };
+
     S.goToPercentage = function(percentage) {
         var pw = S.angler.getValueForPercentage(percentage);
         S.gpioServer.applyPulseWidth(pw, S.pin);
--- two_axis_servo_controller.js
+++ two_axis_servo_controller.js
@@ -1,8 +1,18 @@
-module.exports = function(servoZ, servoX) {
+module.exports = function(servoX, servoZ) {
     var C = {
+        angles : {
+            x : {
+                    min : 45,
+                    max : 135
+                },
+            z : {
+                    min : 45,
+                    max : 135
+            }
+        },
         servoControllers : {
-                x:servoX, z:servoZ
-            },
+            x:servoX, z:servoZ
+        },
         screen {
             width : 0,
             height : 0,
@@ -10,29 +20,39 @@ module.exports = function(servoZ, servoX) {
         }
     };
 
-    C.goToPercentage = function(zAxisPercentage, yAxisPercentage){
+    C.servoControllers.x.setAngleRestrictions(C.angles.x.min, C.angles.x.max);
+    C.servoControllers.z.setAngleRestrictions(C.angles.z.min, C.angles.z.max);
+
+    C.goToPercentage = function(xAxisPercentage, zAxisPercentage){
+        C.servoControllers.x.goToPercentage(xAxisPercentage);
         C.servoControllers.z.goToPercentage(zAxisPercentage);
-        C.servoControllers.y.goToPercentage(yAxisPercentage);
     };
 
     C.goToCoordinate = function(x, y){
+        C.servoControllers.x.goToPercentage(C.getXAxisPercentageForYCoord(y));
         C.servoControllers.z.goToPercentage(C.getZAxisPercentageForXCoord(x));
-        C.servoControllers.y.goToPercentage(C.getYAxisPercentageForYCoord(Y));
     };
 
     C.getZAxisPercentageForXCoord = function(x) {
-
+        x = x > C.screen.width ? C.screen.width : x;
+        x = x < 0 ? 0 : x;
+        return 100 * x / C.screen.width;
     };
 
-    C.getYAxisPercentageForYCoord = function(y) {
-
+    C.getXAxisPercentageForYCoord = function(y) {
+        y = y > C.screen.height ? C.screen.height : y;
+        y = y < 0 ? 0 : y;
+        return 100 - (100 * y / C.screen.height);
     };
 
     C.setScreenDistanceCM = function(distance) {
-
+        C.screen.width = distance * Math.sin(C.angles.z.min);
+        C.screen.width += distance * Math.sin(C.angles.z.max-90);
+        C.screen.height = distance * Math.sin(C.angles.x.min);
+        C.screen.height += distance * Math.sin(C.angles.x.max-90);
     };
 
-    C.setScreenDistance(10);
+    C.setScreenDistanceCM(10);
 
     return C;
 }
