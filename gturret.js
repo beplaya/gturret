be9ddc6772b12630c574c00629f3fd6c27774859
g <gjinfo737@gmail.com>
2016-07-16 16:53:37 -0400

    wip

--- gturret.js
+++ gturret.js
@@ -10,8 +10,8 @@ var screenDimsCM = { x: 10, y: 10};
 
 require(__dirname+'/gpio_server.js')(function(gpioServer) {
 
-	var servoX = require(__dirname+"/servo_controller.js")(gpioServer, 18, "x-axis");
-	var servoZ = require(__dirname+"/servo_controller.js")(gpioServer, 14, "z-axis");
+	var servoX = require(__dirname+"/servo_controller.js")(gpioServer, 17, "x-axis");
+	var servoZ = require(__dirname+"/servo_controller.js")(gpioServer, 18, "z-axis");
 
 	var turret = require(__dirname+"/two_axis_servo_controller.js")(servoX, servoZ);
 
@@ -19,7 +19,13 @@ require(__dirname+'/gpio_server.js')(function(gpioServer) {
 			screenDimsCM.x, screenDimsCM.y);
 
 
-	turret.goToCoordinate(1, 9);
+	setTimeout(function() {turret.goToCoordinate(0, 0)}, 1000);
+	setTimeout(function() {turret.goToCoordinate(10, 10)}, 2000);
+	setTimeout(function() {turret.goToCoordinate(0, 10)}, 3000);
+	setTimeout(function() {turret.goToCoordinate(10, 0)}, 4000);
+	setTimeout(function() {turret.goToCoordinate(5, 5)}, 5000);
+	setTimeout(function() {turret.goToCoordinate(0, 0)}, 6000);
+	
 	
 	gpioServer.end();
 });
