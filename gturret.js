19f173922c710c4d74a7e95577849b570ecf76d0
g <gjinfo737@gmail.com>
2016-07-17 14:43:37 -0400

    wip

--- gturret.js
+++ gturret.js
@@ -13,17 +13,18 @@ require(__dirname+'/gpio_server.js')(function(gpioServer) {
 	turret.setScreenDistanceCM(distanceToScreenCM,
 			screenDimsCM.x, screenDimsCM.y);
 
-	var frequency = 1000;
+	var frequency = 2000;
 	var locations = [];
 	var index = 0;
 	//
 	locations.push([1, 1]);
+	locations.push([10, 1]);
+	locations.push([10, 10]);
+	locations.push([1, 10]);
 	locations.push([1, 1]);
-	locations.push([10,10]);
-	locations.push([10,10]);
-	locations.push([5,5]);
-	locations.push([5,5]);
 	//
+
+    turret.goToCoordinate(0, 0);
 	var servoUpdateInterval = setInterval(function(){
 		//console.log(locations[index][0], locations[index][1]);
 		turret.goToCoordinate(locations[index][0], locations[index][1]);
@@ -31,5 +32,5 @@ require(__dirname+'/gpio_server.js')(function(gpioServer) {
 		index = index >= locations.length ? 0 :index; 
 	}, frequency);
 	//#
-	setTimeout(function() { turret.goToCoordinate(0, 0); gpioServer.end(); clearInterval(servoUpdateInterval)}, 3 * ((locations.length)*frequency));
+	setTimeout(function() { turret.goToCoordinate(0, 0); gpioServer.end(); clearInterval(servoUpdateInterval)}, ((locations.length)*frequency));
 });
