798db0806eae04c0dbcc16b29f6967c9df07ed1b
g <gjinfo737@gmail.com>
2016-07-17 19:15:03 -0400

    wip

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,20 @@ angular.module("angularapp").controller("servoController", function($scope){
 
 function Servo(axis){
     this.axis = axis;
+    this.percent = 0;
+    this.percentStep = 1;
+
+    this.onCCW = function(servo) {
+        console.log("ccw", this.axis);
+        if(this.percent - this.percentStep >= 0){
+            this.percent -= this.percentStep;
+        }
+    };
+
+    this.onCW = function(servo) {
+        console.log("cw", this.axis);
+        if(this.percent + this.percentStep <= 100){
+            this.percent += this.percentStep;
+        }
+    };
 }
