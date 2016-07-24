0779edf7d94aa77a49089a7feb98167013f6bf95
g <gjinfo737@gmail.com>
2016-07-24 18:46:02 -0400

    wip

--- gpio_web_server/lib/camera/gcamera.js
+++ gpio_web_server/lib/camera/gcamera.js
@@ -20,12 +20,7 @@ function GCamera(){
         console.log('raspistill', args.join(" "));
         console.log('');
         this.proc = spawn('raspistill', args);
-//        this.fs.watchFile(this.filePath, function(current, previous) {
-//            var url = 'image_stream.jpg?_t=' + (Math.random() * 100000);
-//            console.log("  >>watchFile<< ", url);
-//            io.sockets.emit('liveStream', url);
-//        });
-        this.streamInterval = setInterval(function(){
+        this.fs.watchFile(this.filePath, function(current, previous) {
             self.fs.readFile(self.filePath, function(err, buf){
                 if(err) {
                     console.log(err);
@@ -37,7 +32,20 @@ function GCamera(){
                     io.sockets.emit('liveStream', { image: true, buffer: buf.toString('base64') });
                 }
             });
-        }, 1000);
+        });
+//        this.streamInterval = setInterval(function(){
+//            self.fs.readFile(self.filePath, function(err, buf){
+//                if(err) {
+//                    console.log(err);
+//                }
+//                if(!buf){
+//                    console.log("Error: buffer empty!");
+//                } else {
+//                    console.log("  >><< ");
+//                    io.sockets.emit('liveStream', { image: true, buffer: buf.toString('base64') });
+//                }
+//            });
+//        }, 1000);
     }
 
     this.stop = function() {
